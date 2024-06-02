fa594d9c759da26666836d17e36a0674bd6f569d
neo66S <160108743+pulseP1986@users.noreply.github.com>
2024-06-02 08:08:08 +0300

    Fix hide info not submiting

--- application/plugins/hide_info/views/webzen_new/view.index.php
+++ application/plugins/hide_info/views/webzen_new/view.index.php
@@ -25,39 +25,39 @@
 							});
 						</script>
 						<form method="post" action="" id="hide_info">
-						<table class="table dmn-rankings-table table-striped">
-							<thead>
-							<tr>
-								<th style="text-align: left;padding-left: 15px;" colspan="3"><?php echo __('Details'); ?></th>
-							</tr>
-							</thead>
-							<tbody>
-							<tr>
-								<td style="width:30%;text-align: left;padding-left: 15px;"><?php echo __('Hide Till'); ?></td>
-								<td style="width:70%;text-align: left;padding-left: 15px;" id="hide_time"><?php echo ($hide_time == false) ? __('None') : $hide_time; ?></td>
-							</tr>
-							<tr>
-								<td style="width:30%;text-align: left;padding-left: 15px;"><?php echo __('Hide Info'); ?></td>
-								<td style="width:70%;text-align: left;padding-left: 15px;"><?php echo __('Everyone can\'t see location or inventory items on all chars'); ?></td>
-							</tr>
-							<tr>
-								<td style="width:30%;text-align: left;padding-left: 15px;"><?php echo __('Hide Price'); ?></td>
-								<td style="width:70%;text-align: left;padding-left: 15px;">
-									<?php
-										$price = $plugin_config['price'];
-										if($this->session->userdata('vip')){
-											$price -= ($price / 100) * $this->session->userdata(['vip' => 'hide_info_discount']);
-										}
-										echo $price; ?> <?php echo $this->website->translate_credits($plugin_config['payment_method'], $this->session->userdata(['user' => 'server'])); ?>
-									, <?php echo $plugin_config['days']; ?> <?php echo __('days'); ?>
-								</td>
-							</tr>
-							</tbody>
-						</table>
+							<table class="table dmn-rankings-table table-striped">
+								<thead>
+								<tr>
+									<th style="text-align: left;padding-left: 15px;" colspan="3"><?php echo __('Details'); ?></th>
+								</tr>
+								</thead>
+								<tbody>
+								<tr>
+									<td style="width:30%;text-align: left;padding-left: 15px;"><?php echo __('Hide Till'); ?></td>
+									<td style="width:70%;text-align: left;padding-left: 15px;" id="hide_time"><?php echo ($hide_time == false) ? __('None') : $hide_time; ?></td>
+								</tr>
+								<tr>
+									<td style="width:30%;text-align: left;padding-left: 15px;"><?php echo __('Hide Info'); ?></td>
+									<td style="width:70%;text-align: left;padding-left: 15px;"><?php echo __('Everyone can\'t see location or inventory items on all chars'); ?></td>
+								</tr>
+								<tr>
+									<td style="width:30%;text-align: left;padding-left: 15px;"><?php echo __('Hide Price'); ?></td>
+									<td style="width:70%;text-align: left;padding-left: 15px;">
+										<?php
+											$price = $plugin_config['price'];
+											if($this->session->userdata('vip')){
+												$price -= ($price / 100) * $this->session->userdata(['vip' => 'hide_info_discount']);
+											}
+											echo $price; ?> <?php echo $this->website->translate_credits($plugin_config['payment_method'], $this->session->userdata(['user' => 'server'])); ?>
+										, <?php echo $plugin_config['days']; ?> <?php echo __('days'); ?>
+									</td>
+								</tr>
+								</tbody>
+							</table>
+							<div class="text-center">
+								<button type="submit" class="btn btn-primary"><?php echo __('Buy Now'); ?></button>
+							</div>
 						</form>
-						<div class="text-center">
-							<button type="submit" class="btn btn-primary"><?php echo __('Buy Now'); ?></button>
-						</div>
 					</div>
 				</div>	
 			</div>	
